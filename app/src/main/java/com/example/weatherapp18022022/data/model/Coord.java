597cb2bef562cc4e62422865a1bc1d51ce8d7608
Phat <phatdroid94@gmail.com>
2022-05-16 18:55:15 +0700

    model

---
+++ app/src/main/java/com/example/weatherapp18022022/data/model/Coord.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp18022022.data.model;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Coord {
+
+@SerializedName("lon")
+@Expose
+private Float lon;
+@SerializedName("lat")
+@Expose
+private Float lat;
+
+public Float getLon() {
+return lon;
+}
+
+public void setLon(Float lon) {
+this.lon = lon;
+}
+
+public Float getLat() {
+return lat;
+}
+
+public void setLat(Float lat) {
+this.lat = lat;
+}
+
+}
