b120a4db80b1a8eda274841db53bc9675c358cd1
Phat <phatdroid94@gmail.com>
2022-05-18 18:29:58 +0700

    finish

--- app/src/main/java/com/example/weatherapp18022022/presentation/activities/MainActivity.java
+++ app/src/main/java/com/example/weatherapp18022022/presentation/activities/MainActivity.java
@@ -8,7 +8,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.weatherapp18022022.R;
 import com.example.weatherapp18022022.data.model.Forecast;
 import com.example.weatherapp18022022.data.model.Main;
@@ -17,14 +19,21 @@ import com.example.weatherapp18022022.presentation.viewmodel.MainViewModel;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     MainViewModel mainViewModel;
+    ImageView imgDetailState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgDetailState = findViewById(R.id.imgDetailState);
+
+        Glide.with(this)
+                .load("http://openweathermap.org/img/wn/10d@2x.png")
+                .into(imgDetailState);
 
         mainViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
             @NonNull
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         mainViewModel.getForecastData().observe(this, new Observer<Forecast>() {
             @Override
             public void onChanged(Forecast forecast) {
+
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
                 Log.d("BBB",simpleDateFormat.format(forecast.getCurrentTime() * 1000) + "");
             }
